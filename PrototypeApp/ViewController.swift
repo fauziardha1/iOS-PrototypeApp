4091aabc53e9873f50f7c51a7536106d840c95ce
Fauzi Arda <fauziardasaputra@gmail.com>
2024-07-09 17:26:35 +0700

    Initial Commit

---
+++ PrototypeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrototypeApp
+//
+//  Created by Fauzi Arda on 09/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
