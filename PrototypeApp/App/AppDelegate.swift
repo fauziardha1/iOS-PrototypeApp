81d3bdaf912cb17638bf8105edf3460a1d34cd94
fauziardha1 <fauziardasaputra@gmail.com>
2025-07-11 02:02:11 +0700

    remove unused code

--- PrototypeApp/App/AppDelegate.swift
+++ PrototypeApp/App/AppDelegate.swift
@@ -45,36 +45,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         print("App will terminate.")
         // Save data if appropriate
     }
-    
-    private func makeViewController() -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
-        window?.makeKeyAndVisible()
-        return true
-    }
-    
-    private func makeSplitView() -> Bool {
-        let splitViewController = UISplitViewController()
-                
-        // Create the master (menu) view controller
-        let menuViewController = MenuViewController()
-        let menuNavController = UINavigationController(rootViewController: menuViewController)
-        
-        // Create the detail (content) view controller
-        let detailViewController = ContentViewController()
-        let detailNavController = UINavigationController(rootViewController: detailViewController)
-        
-        // Set the view controllers of the split view controller
-        splitViewController.viewControllers = [menuNavController, detailNavController]
-        splitViewController.preferredDisplayMode = .automatic
-        menuViewController.selectFirstMenu()
-        
-        // Assign the split view controller as the window's root view controller
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = splitViewController
-        window?.makeKeyAndVisible()
-        return true
-    }
 
 }
 
